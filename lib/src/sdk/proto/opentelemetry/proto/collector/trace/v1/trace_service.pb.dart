9b311805748072ac1cae67403a237a6497da9156
developersteve <steven@developersteve.com>
2023-08-16 18:47:21 +1000

    Update trace_service.pb.dart

--- lib/src/sdk/proto/opentelemetry/proto/collector/trace/v1/trace_service.pb.dart
+++ lib/src/sdk/proto/opentelemetry/proto/collector/trace/v1/trace_service.pb.dart
@@ -1,13 +1,3 @@
-// Copyright 2021-2022 Workiva.
-// Licensed under the Apache License, Version 2.0. Please see https://github.com/Workiva/opentelemetry-dart/blob/master/LICENSE for more information
-
-///
-//  Generated code. Do not modify.
-//  source: opentelemetry/proto/collector/trace/v1/trace_service.proto
-//
-// @dart = 2.7
-// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
-
 import 'dart:async' as $async;
 import 'dart:core' as $core;
 
